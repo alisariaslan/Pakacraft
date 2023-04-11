e4c6ab97278c686ea776b10d6048444534e22a72
alisariaslan <acsayear@gmail.com>
2023-04-11 07:36:14 +0300

    t

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-node main.js 192.168.99.36 80 mongodb://127.0.0.1:27017/Pakacraft 4 paper-1.19.3-368.jar
+node main.js 192.168.1.100 80 mongodb://127.0.0.1:27017/Pakacraft 4 paper-1.19.3-368.jar
 :: pause
