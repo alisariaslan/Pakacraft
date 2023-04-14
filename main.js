bf6f74c88bc84713d100421874ab05245980ce07
alisariaslan <acsayear@gmail.com>
2023-04-14 19:11:23 +0300

    http & https options

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 //ARGV-ARGC
 var myArgs = process.argv.slice(2);
-if (myArgs.length != 5)
-  return console.log("Yetersiz argüman! (Ip, Port, MongoDB, Ram, Target)")
+if (myArgs.length < 6)
+  return console.log('Invalid Args! Example: "node main.js localhost 80 mongodb://127.0.0.1:27017/Minecraft 4 server.jar -http"');
 
 var listen_ip = myArgs[0];
 var listen_port = myArgs[1];
-
-//MY MODULES
-var my_date = require('./my_modules/my_date');
-
-//START SUB SCRIPTS
-var XML = require('./src/xml');
-var CMD = require('./src/cmd');
-var DB = require('./src/db');
-var JAVA = require('./src/java');
+var htt_type = myArgs[5];
 
 //MODULES
 var express = require('express');
@@ -22,6 +14,9 @@ var bodyParser = require('body-parser');
 var routes = require('./router/router');
 var https = require('https');
 var fs = require('fs');
+
+//MY MODULES
+var my_date = require('./my_modules/my_date');
 
 //USE
 app.use(express.static('public'));
@@ -37,11 +32,45 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 //HTTPS SERVER
-const server = https.createServer({
-  key: fs.readFileSync('ssl/key.pem'),
-  cert: fs.readFileSync('ssl/cert.pem'),
-  address: listen_ip,
-  port: listen_port
-}, app);
+function start_https() {
+  if (myArgs.length < 8) {
+    console.log('Invalid Args! Example: "node main.js localhost 80 mongodb://127.0.0.1:27017/Minecraft 4 server.jar -https key.ex cert.ex" ');
+    return;
+  }
+  var ssl_key = myArgs[6];
+  var ssl_cert = myArgs[7];
+  const server = https.createServer({
+    key: fs.readFileSync('ssl/' + ssl_key),
+    cert: fs.readFileSync('ssl/' + ssl_cert),
+    ca: myArgs[8] != null ? fs.readFileSync('ssl/' + myArgs[8]) : null,
+    address: listen_ip,
+    port: listen_port
+  }, app);
+  server.listen(443);
+  console.log('Web sunucusu şu adreste çalışmaya başladı -> https://%s:%s', listen_ip, listen_port);
+  start_subscripts();
+}
 
-server.listen(443);
+function start_http() {
+  var server = app.listen(listen_port, listen_ip, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Web sunucusu şu adreste çalışmaya başladı -> http://%s:%s', host, port);
+    start_subscripts();
+  });
+}
+
+if (htt_type.includes('-https'))
+  start_https();
+else if (htt_type.includes('-http'))
+  start_http();
+else
+  console.log('Invalid Args! Example: "node main.js localhost 80 -http / -https');
+
+function start_subscripts() {
+
+  var XML = require('./src/xml');
+  var CMD = require('./src/cmd');
+  var DB = require('./src/db');
+  var JAVA = require('./src/java');
+}
--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-node main.js 192.168.1.100 80 mongodb://127.0.0.1:27017/Pakacraft 4 paper-1.19.3-368.jar
+node main.js localhost 80 mongodb://127.0.0.1:27017/Pakacraft 4 paper-1.19.3-368.jar -https openssl/key.pem openssl/cert.pem 
 :: pause
